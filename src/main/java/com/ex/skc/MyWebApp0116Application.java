1eea9442ca7ece821a005b3f9dfc025eac7ba14e
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-01-16 09:34:49 +0900

    Initial commit

---
+++ src/main/java/com/ex/skc/MyWebApp0116Application.java
@@ -0,0 +1,13 @@
+package com.ex.skc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebApp0116Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebApp0116Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ex/skc/MyWebApp0116ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.skc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebApp0116ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
